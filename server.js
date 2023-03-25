380237f5bb78fd53837deed8066277bf90024357
DaveyAuz <doswald27@gmail.com>
2023-03-24 23:16:32 -0400

    adding more back end code

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+
 'use strict';
 
 const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
+const cors = require('cors');
+const getPhotos = require('./modules/photos.js');
 const app = express();
 
+app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (request, response) => {
@@ -13,7 +16,24 @@ app.get('/', (request, response) => {
     response.status(200).send('Welcome to my Server!');
 });
 
-app.get('/weather', (request, response) => {)
+app.listen(PORT, () => console.log(`We are running on port ${PORT}!`));
+
+app.get('/photos', getPhotos);
+
+// *** CATCH ALL - AT THE BOTTOM AND SERVE AS A 404 ERROR MESSAGE
+app.get('*', (request, response) => {
+  response.status(404).send('This route does not exist');
+});
+
+// **** ERROR HANDLING - PLUG AND PLAY CODE FROM EXPRESS DOCS ****
+app.use((error, request, response, next) => {
+  console.log(error.message);
+  response.status(500).send(error.message);
+});
+
+
+
+app.get('/weather', (request, response, next) => {)
 
 let url = 'http://api.weatherbit.io/v2.0/forecast/daily/?key=1df3370cd0124f7484df5f36bed32ce6&land=en&lat=47.6062&lon=-122.3321&days=5'
 
@@ -21,40 +41,40 @@ let url = 'http://api.weatherbit.io/v2.0/forecast/daily/?key=1df3370cd0124f7484d
 //     try keywordFromFrontEnd = request.query.keyword;
 // }
 
-//TODO: BUILD AND ENDPOINT THAT WILL CALL OUT TO AN API
-// app.get('/photos', async (request, response, next) => {
-//     try {
-//         //TODO: accept my queries -> /photos?query=Value
-//         let keywordFromFrontEnd = request.query.searchQuery;
+TODO: BUILD AND ENDPOINT THAT WILL CALL OUT TO AN API
+app.get('/photos', async (request, response, next) => {
+    try {
+        //TODO: accept my queries -> /photos?query=Value
+        let keywordFromFrontEnd = request.query.searchQuery};
 
-//         //TODO: Build my URL from Axios
-//         let URL = `https://api.unsplash.com/search/photos?query=${keywordFromFrontEnd
+        //TODO: Build my URL from Axios
+        let URL = `https://api.unsplash.com/search/photos?query=${keywordFromFrontEnd}
+    }
+    }
+}
+{
+            app.length('/weather', (request, response, nest0 => 
+    } try {
+            // 
+            // TODO: accept queries - lat, lon, searchQuery
+            let lat = request.query.lat;
+            let lon = request.query.lon;
+            let cityName = request.query.searchQuery
+
+            console.log(request.query);
+
+            //TODO: Find that city based on cityName
+
+            let city = weather.find(city => city.cityName.toLowerCase() === cityName.toLowerCase());
 
 
+            response.status(200).send(city);
 
-// {
-//             app.length('/weather', (request, response, nest0 => 
-//     } try {
-//             // 
-//             // TODO: accept queries - lat, lon, searchQuery
-//             let lat = request.query.lat;
-//             let lon = request.query.lon;
-//             let cityName = request.query.searchQuery
-
-//             console.log(request.query);
-
-//             //TODO: Find that city based on cityName
-
-//             let city = weather.find(city => city.cityName.toLowerCase() === cityName.toLowerCase());
-
-
-//             response.status(200).send(city);
-
-//         } catch (error) {
-//             next(error);
-//         }
-//     });
-
+        } catch (error) {
+            next(error);
+        }
+    });
+    }
 //TODO: BUILD ANOTHER CLASS TO TRIM DOWN THE DATA
 class Photo {
     constructor(picObj){
@@ -80,4 +100,5 @@ class Photo {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+    }
 
